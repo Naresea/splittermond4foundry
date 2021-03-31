05e321b01471d01fd4073ce058e5035d97a37c1e
Sandra Wagner <sandra.sawa.wagner@gmail.com>
2021-03-31 15:22:35 +0100

    more generic item sheets

--- src/module/models/items/abstammung.ts
+++ src/module/models/items/abstammung.ts
@@ -1,6 +1,7 @@
 import {Quelle} from './quelle';
 import {Info} from './info';
+import {Modifier} from './modifier';
 
 export interface Abstammung extends Info, Quelle {
-
+    modifier: Array<Modifier>;
 }
--- src/module/models/items/kultur.ts
+++ src/module/models/items/kultur.ts
@@ -1,7 +1,9 @@
 import {Quelle} from './quelle';
 import {Info} from './info';
+import {Modifier} from './modifier';
 
 export interface Kultur extends Info, Quelle {
     sprache: string;
     kulturkunde: string;
+    modifier: Array<Modifier>;
 }
