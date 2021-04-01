8fee5ecf716782c72d15c78aa3fac5bdbafc12b7
Sandra Wagner <sandra.sawa.wagner@gmail.com>
2021-04-01 16:26:37 +0100

    hide next and prev round / turn buttons

--- src/module/combat/register-combat.ts
+++ src/module/combat/register-combat.ts
@@ -2,4 +2,14 @@ import {SplimoCombat} from './splimo-combat';
 
 export function registerCombat(): void {
     CONFIG.Combat.entityClass = SplimoCombat;
+
+    Hooks.on('renderCombatTracker', async (app, html, options) => {
+        const elems = [
+            html.find('.combat-control[data-control="previousRound"]'),
+            html.find('.combat-control[data-control="nextRound"]'),
+            html.find('.combat-control[data-control="previousTurn"]'),
+            html.find('.combat-control[data-control="nextTurn"]'),
+        ];
+        elems.forEach(e => e.hide());
+    });
 }
